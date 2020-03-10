790a3b24a26f819801e2eca6006535a61556cae9
Wasim Alayoubi <wasim.alayoubi@mail.concordia.ca>
2020-03-10 11:57:39 -0400

    created Maven project artifacts

---
+++ sequencer/src/main/java/ca/concordia/ginacody/comp6231/demsha/App.java
@@ -0,0 +1,13 @@
+package ca.concordia.ginacody.comp6231.demsha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
