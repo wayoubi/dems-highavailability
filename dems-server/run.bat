2d631eef6026abe91801668295f5fc7f3077c615
Wasim Alayoubi <wasim.alayoubi@mail.concordia.ca>
2020-03-30 22:20:35 -0400

    prefer ipv43

--- dems-server/run.bat
+++ dems-server/run.bat
@@ -3,4 +3,4 @@ echo Starting DEMS Server
 call mvn install
 call cls
 call cd target
-call java -jar dems-server-1.0-SNAPSHOT.jar %1 %2 %3 %4 %5
+call -Djava.net.preferIPv4Stack=true java -jar dems-server-1.0-SNAPSHOT.jar %1 %2 %3 %4 %5
--- frontend/run.bat
+++ frontend/run.bat
@@ -3,4 +3,4 @@ echo Starting Frontend Server
 call mvn install
 call cls
 call cd target
-call java -jar frontend-1.0-SNAPSHOT.jar %1 %2 %3 %4 %5 %6
+call -Djava.net.preferIPv4Stack=true java -jar frontend-1.0-SNAPSHOT.jar %1 %2 %3 %4 %5 %6
