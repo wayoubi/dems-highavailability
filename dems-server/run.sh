7b7a42337d80bdc568ac2bcde7394b9786d849eb
Wasim Alayoubi <wasim.alayoubi@mail.concordia.ca>
2020-04-03 00:38:04 -0400

    added registry policy to dems server running script

--- dems-server/run.sh
+++ dems-server/run.sh
@@ -1,4 +1,4 @@
 mvn clean install
 clear
 cd target
-java -Djava.net.preferIPv4Stack=true -jar dems-server-1.0-SNAPSHOT.jar $1 $2 $3 $4 $5
+java -Djava.net.preferIPv4Stack=true -Djava.security.policy=$DEMSHA_HOME/rmiregistry.policy -jar dems-server-1.0-SNAPSHOT.jar $1 $2 $3 $4 $5
